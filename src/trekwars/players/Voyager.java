8fba8993b01fde95f5d13bf11d2faad98cfa6381
Andy Kuszyk <andykuszyk@live.co.uk>
2016-12-22 11:41:16 +0800

    wip on rotations

--- src/trekwars/players/Voyager.java
+++ src/trekwars/players/Voyager.java
@@ -2,18 +2,20 @@ package trekwars.players;
 
 import com.jme3.asset.AssetManager;
 import com.jme3.material.Material;
-import com.jme3.scene.Node;
+import com.jme3.math.Quaternion;
 import com.jme3.scene.Spatial;
 
 public class Voyager extends AbstractPlayer {
     
+    private final Spatial _voyager;
+    
     public Voyager(AssetManager assetManager){
-        Spatial voyager = assetManager.loadModel("Models/Voyager.obj");
+        _voyager = assetManager.loadModel("Models/Voyager.obj");
         Material mat_default = new Material(assetManager, "Common/MatDefs/Misc/ShowNormals.j3md");
-        voyager.setMaterial(mat_default);
-        voyager.setLocalScale(1);
+        _voyager.setMaterial(mat_default);
+        _voyager.setLocalScale(1);
         
-        attachChild(voyager);
+        attachChild(_voyager);
         getRootNode().setLocalTranslation(0, 0, -10);
     }
 
@@ -35,5 +37,82 @@ public class Voyager extends AbstractPlayer {
     protected float getBoostCapacity() {
         return 1;
     }
+
+    @Override
+    protected void onTurn(TurnDirection turnDirection, float tpf) {
+        Quaternion localRotation = _voyager.getLocalRotation();
+        System.out.println(String.format(
+                "Local rotation is XYZ (%1$f, %2$f, %3$f)", 
+                localRotation.getX(), 
+                localRotation.getY(), 
+                localRotation.getZ()));
+        
+        switch(turnDirection){
+            case Right:
+                if(localRotation.getZ() > -0.5) {
+                    _voyager.rotate(0,0,-getRotationalSpeed() * tpf);
+                    //decreaseZRotation(localRotation, tpf);
+                } 
+//                else {
+//                    setZRotation(localRotation, (float)(0.9 * Math.PI));
+//                }
+                break;
+            case Left:
+                if(localRotation.getZ() < 0.5){
+                    _voyager.rotate(0,0,getRotationalSpeed() * tpf);
+                    //increaseZRotation(localRotation, tpf);
+                } 
+//                else {
+//                    setZRotation(localRotation, (float)(Math.PI * 3 / 4));
+//                }
+                break;
+            default:
+//                if(localRotation.getZ() > 0 && localRotation.getZ() < Math.PI){
+//                    decreaseZRotation(localRotation, tpf);
+//                } else if(localRotation.getZ() > Math.PI) {
+//                    increaseZRotation(localRotation, tpf);
+//                }
+//                setZRotation(localRotation, 0);
+                break;
+        }
+    }
     
+    private void setZRotation(Quaternion localRotation, float z) {
+        System.out.println(String.format("Setting Z rotation to %1$f", z));
+        _voyager.rotate(localRotation.getX(), localRotation.getY(), z);
+//        _voyager.setLocalRotation(new Quaternion(
+//                localRotation.getX(),
+//                localRotation.getY(),
+//                z,
+//                localRotation.getW()));
+    }
+    
+    private void increaseZRotation(Quaternion localRotation, float tpf){
+        _voyager.rotate(
+        //_voyager.setLocalRotation(new Quaternion(
+                localRotation.getX(),
+                localRotation.getY(),
+                wrapRadians(localRotation.getZ() + getRotationalSpeed() * tpf));
+          //      localRotation.getW()));
+    }
+    
+    private void decreaseZRotation(Quaternion localRotation, float tpf){
+        _voyager.rotate(
+        //_voyager.setLocalRotation(new Quaternion(
+                localRotation.getX(),
+                localRotation.getY(),
+                wrapRadians(localRotation.getZ() - getRotationalSpeed() * tpf));
+          //      localRotation.getW()));
+    }
+    
+    private float wrapRadians(float radians) {
+        return radians;
+//        if(radians > Math.PI * 2){
+//            return (float)(radians - (Math.PI * 2));
+//        } else if (radians < 0){
+//            return (float)(radians + (Math.PI * 2));
+//        } else {
+//            return radians;
+//        }
+    }
 }
