e494bfc181aad9d740ac441154776e29b5cf2e89
Andy Kuszyk <andykuszyk@live.co.uk>
2016-12-26 10:49:41 +0800

    Implemented distance calculation

---
+++ src/trekwars/core/Distance.java
@@ -0,0 +1,22 @@
+package trekwars.core;
+
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Spatial;
+
+public class Distance {
+    private final double _local;
+    
+    public Distance(Spatial a, Spatial b) {
+        Vector3f aLocal = a.getLocalTranslation();
+        Vector3f bLocal = b.getLocalTranslation();
+        
+        _local = Math.sqrt(
+                Math.pow(aLocal.getX() - bLocal.getX(), 2) + 
+                Math.pow(aLocal.getY() - bLocal.getY(), 2) + 
+                Math.pow(aLocal.getZ() - bLocal.getZ(), 2));
+    }
+    
+    public double getLocal() {
+        return _local;
+    }
+}
