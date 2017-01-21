6446c1b210de8f2ae04e78b66d087e36b4c661d3
Andy Kuszyk <andykuszyk@live.co.uk>
2017-01-21 16:07:42 +0000

    Added android support

---
+++ mobile/gen/R.java.d
@@ -0,0 +1,12 @@
+/home/andy/code/trekwars/mobile/gen/com/szyk/trekwars/R.java \
+ : /home/andy/code/trekwars/mobile/res/drawable-mdpi/ic_launcher.png \
+/home/andy/code/trekwars/mobile/res/layout/main.xml \
+/home/andy/code/trekwars/mobile/res/drawable-hdpi/ic_launcher.png \
+/home/andy/code/trekwars/mobile/res/drawable-ldpi/ic_launcher.png \
+/home/andy/code/trekwars/mobile/res/values/strings.xml \
+/home/andy/code/trekwars/mobile/res/drawable-xhdpi/ic_launcher.png \
+/home/andy/code/trekwars/mobile/bin/res/drawable-mdpi/ic_launcher.png \
+/home/andy/code/trekwars/mobile/bin/res/drawable-hdpi/ic_launcher.png \
+/home/andy/code/trekwars/mobile/bin/res/drawable-ldpi/ic_launcher.png \
+/home/andy/code/trekwars/mobile/bin/res/drawable-xhdpi/ic_launcher.png \
+/home/andy/code/trekwars/mobile/bin/AndroidManifest.xml \
